26c3224b2645dc85cb3f90910e1ce7108e7095f5
Kent Overstreet <kent.overstreet@linux.dev>
2025-06-11 00:26:06 -0400

    Update bcachefs sources to 5e40e376e242 bcachefs: Allow CONFIG_UNICODE=m
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- libbcachefs/sb-members.c
+++ libbcachefs/sb-members.c
@@ -325,12 +325,9 @@ static void bch2_sb_members_v1_to_text(struct printbuf *out, struct bch_sb *sb,
 {
 	struct bch_sb_field_members_v1 *mi = field_to_type(f, members_v1);
 	struct bch_sb_field_disk_groups *gi = bch2_sb_field_get(sb, disk_groups);
-	int nr = (vstruct_end(&mi->field) - (void *) &gi->entries[0]) / sizeof(gi->entries[0]);
+	unsigned i;
 
-	if (nr != sb->nr_devices)
-		prt_printf(out, "nr_devices mismatch: have %i entries, should be %u", nr, sb->nr_devices);
-
-	for (int i = 0; i < nr; i++)
+	for (i = 0; i < sb->nr_devices; i++)
 		member_to_text(out, members_v1_get(mi, i), gi, sb, i);
 }
 
@@ -344,17 +341,9 @@ static void bch2_sb_members_v2_to_text(struct printbuf *out, struct bch_sb *sb,
 {
 	struct bch_sb_field_members_v2 *mi = field_to_type(f, members_v2);
 	struct bch_sb_field_disk_groups *gi = bch2_sb_field_get(sb, disk_groups);
-	int nr = (vstruct_end(&mi->field) - (void *) &gi->entries[0]) / le16_to_cpu(mi->member_bytes);
+	unsigned i;
 
-	if (nr != sb->nr_devices)
-		prt_printf(out, "nr_devices mismatch: have %i entries, should be %u", nr, sb->nr_devices);
-
-	/*
-	 * We call to_text() on superblock sections that haven't passed
-	 * validate, so we can't trust sb->nr_devices.
-	 */
-
-	for (int i = 0; i < nr; i++)
+	for (i = 0; i < sb->nr_devices; i++)
 		member_to_text(out, members_v2_get(mi, i), gi, sb, i);
 }
 
